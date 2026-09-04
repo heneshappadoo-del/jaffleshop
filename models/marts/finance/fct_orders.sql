280be66cc0a72ca693e5b2f4517cffd62e39955f
Henesh APPADOO <vidishappadoo.official@gmail.com>
2026-09-04 06:44:36 +0000

    before mesh

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,3 +1,9 @@
+{{ config(
+    materialized='incremental',
+    unique_key='order_id',
+    incremental_strategy='merge'
+) }}
+
 with orders as (
 
     select *
@@ -16,11 +22,11 @@ order_payments as (
 
     select
         order_id,
-        sum(amount) as amount
+        sum(payment_amount) as amount
 
     from payments
 
-    where status = 'success'
+    where payment_status <> 'fail'
 
     group by 1
 
@@ -31,12 +37,26 @@ final as (
     select
         orders.order_id,
         orders.customer_id,
+        orders.order_date,
         coalesce(order_payments.amount, 0) as amount
 
     from orders
 
     left join order_payments
         on orders.order_id = order_payments.order_id
+
+    {% if is_incremental() %}
+
+        where orders.order_date >
+        (
+            select coalesce(
+                max(order_date),
+                to_date('1900-01-01')
+            )
+            from {{ this }}
+        )
+
+    {% endif %}
 
 )
 
